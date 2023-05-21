4c5cb3468b019ca0ebeaaa7dbb0d35d0cc0aa5ad
Viral3301 <okxolod2@gmail.com>
2023-05-21 22:23:59 +0300

    Switched psycopg2 to asyncpg

--- database.py
+++ database.py
@@ -1,12 +1,23 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:223344@localhost/fastapi_eshop"
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:223344@localhost/fastapi_eshop"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 Base = declarative_base()
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:223344@localhost/fastapi_eshop"
--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI,Request,Depends,File,UploadFile
 from fastapi.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from sqlalchemy.orm import Session
-from database import SessionLocal, engine
-from eshop import models
+from database import AsyncSession, engine,async_session
+from eshop.models import *
 import shutil
+from sqlalchemy import select
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/content_img", StaticFiles(directory='content_img'),name='content_img')
@@ -16,49 +17,24 @@ app.mount("/content_img", StaticFiles(directory='content_img'),name='content_img
 templates = Jinja2Templates(directory="templates")
 
 
-def get_db():
-    db = SessionLocal()
-    try: 
-        yield db
-    finally:
-        db.close()
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
 
 
 @app.get("/")
-async def home(request: Request,db: SessionLocal = Depends(get_db)):
-    backpacks = db.query(models.Accessories).filter(models.Accessories.category == 1)
-    swimsuits = db.query(models.Accessories).filter(models.Accessories.category == 2)
+async def home(request: Request,session: AsyncSession = Depends(get_session)):
+    raw_backpacks = await session.execute(select(Accessories))
+    raw_swimsuits = await session.execute(select(Accessories).where(Accessories.category == 2))
+    backpacks = raw_backpacks.scalars().all()
+    swimsuits = raw_swimsuits.scalars().all()
     return templates.TemplateResponse('home.html',{'request':request,'backpacks': backpacks,'swimsuits':swimsuits})
 
-@app.post("/create")
-async def CreateAcessory(request: Request,title: str,price: int,manufacturer: str ,amount_in_stock: int,sale: bool,category:int,image: UploadFile = File(...),db: SessionLocal = Depends(get_db)):
-    with open (f'content_img/{image.filename}',"wb") as buffer:
-        shutil.copyfileobj(image.file,buffer)
-    new_accesory = models.Accessories(title=title,image=image.filename,price=price,manufacturer=manufacturer,amount_in_stock=amount_in_stock,sale=sale,category=category)
-    db.add(new_accesory)
-    db.commit()
-    return {'response': "good"}
-
-@app.post("/createcategory")
-async def CreateCategory(request: Request,name: str,db: SessionLocal = Depends(get_db)):
-    new_category = models.Category(name=name)
-    db.add(new_category)
-    db.commit()
-    return {'response': "good"}
-
-@app.post("/createvehicle")
-async def CreateVehicle(request: Request,title: str,price: int,manufacturer: str ,seats:int,engine:int,engine_type: str,year:int,amount_in_stock: int,sale: bool,category:int,image: UploadFile = File(...),db: SessionLocal = Depends(get_db)):
-    with open (f'content_img/{image.filename}',"wb") as buffer:
-        shutil.copyfileobj(image.file,buffer)
-    new_vehicle = models.Vehicles(title=title,image=image.filename,price=price,manufacturer=manufacturer,seats=seats,engine=engine,engine_type=engine_type,year=year,amount_in_stock=amount_in_stock,sale=sale,category=category)
-    db.add(new_vehicle)
-    db.commit()
-    return {'response': "good"}
-
 @app.get("/category/{category_id}/{page_num}/")
-async def home(request: Request, category_id: int,page_num: int = 1,db: SessionLocal = Depends(get_db)):
-    data = db.query(models.Vehicles).filter(models.Vehicles.category == category_id)
-    data_length = data.count()
+async def home(request: Request, category_id: int,page_num: int = 1,session: AsyncSession = Depends(get_session)):
+    raw_data = await session.execute(select(Vehicles).where(Vehicles.category == category_id))
+    data = raw_data.scalars().all()
+    data_length = len(data)
     if data_length % 12 == 0:
         pages_total = data_length // 12
     else:
@@ -68,6 +44,15 @@ async def home(request: Request, category_id: int,page_num: int = 1,db: SessionL
     return templates.TemplateResponse('catalog.html',{'request':request,'data': data[start:end],'pages_total':pages_total+1,"category_id":category_id,"page_num": page_num},)
 
 @app.get("/product/{product_id}")
-async def home(request: Request,product_id: int,db: SessionLocal = Depends(get_db)):
-    product = db.query(models.Vehicles).filter(models.Vehicles.id == product_id)
+async def home(request: Request,product_id: int,session: AsyncSession = Depends(get_session)):
+    raw_product = await session.execute(select(Vehicles).where(Vehicles.id == product_id))
+    product = raw_product.scalars().all()
     return templates.TemplateResponse('product-page.html',{'request':request,'product':product})
+
+@app.get("/login")
+async def home(request: Request,session: AsyncSession = Depends(get_session)):
+    return templates.TemplateResponse('login.html',{'request':request})
+
+@app.get("/registration")
+async def home(request: Request,session: AsyncSession = Depends(get_session)):
+    return templates.TemplateResponse('registration.html',{'request':request})
